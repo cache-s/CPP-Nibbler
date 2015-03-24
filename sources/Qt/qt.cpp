57fb57d1c613204a98794e3c4c82bd240d44e689
charie_p <charie_p@git.epitech.eu>
2015-03-24 17:50:59 +0100

    daa

---
+++ sources/Qt/qt.cpp
@@ -0,0 +1,12 @@
+#include "qt.h"
+
+Qt::Qt()
+{
+    QApplication app(argc, argv);
+    app.exec();
+}
+
+Qt::~Qt()
+{
+
+}
---
+++ sources/Qt/qt.h
@@ -0,0 +1,14 @@
+#ifndef QT_H
+# define QT_H
+
+# include <QApplication>
+# include <QPushButton>
+
+class Qt
+{
+public:
+    Qt();
+    ~Qt();
+};
+
+#endif // QT_H
