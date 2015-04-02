31d26aa51c523ad4f509aa0aa823a6613dab7930
charie_p <charie_p@git.epitech.eu>
2015-04-02 17:45:38 +0200

    dada

--- sources/Xlib/Lib_Xlib.cpp
+++ sources/Xlib/Lib_Xlib.cpp
@@ -5,7 +5,7 @@
 // Login   <charie_p@epitech.net>
 //
 // Started on  Thu Apr  2 17:08:53 2015 Pierre Charié
-// Last update Thu Apr  2 17:08:54 2015 Pierre Charié
+// Last update Thu Apr  2 17:16:33 2015 Pierre Charié
 //
 
 #include	<sstream>
@@ -186,15 +186,15 @@ void            Xlib::display(data const d)
 	      case 6:
 	      	this->draw_rect(x * PIXSIZE, y * PIXSIZE, _gcObst);
 		break;
-	      }
 	      default:
 		break;
 	      }
-	  }
-std::ostringstream oss;
-oss << "Score : " << d.score;
-XDrawString(_disp, _win, _gcObst, 10, 9, oss.str().c_str(), strlen(oss.str().c_str()));
-_oldScore = d.score;
+      }
+  XSync(_disp, false);
+  std::ostringstream oss;
+  oss << "Score : " << d.score;
+  XDrawString(_disp, _win, _gcObst, 10, 9, oss.str().c_str(), strlen(oss.str().c_str()));
+  _oldScore = d.score;
   XSync(_disp, false);
 }
 
